9b1b82e94c467b7e46f2f15af0b9abcbc83649d6
GwFirman <firmanzamzamiaziz@gmail.com>
2024-10-01 15:21:24 +0700

    Fix  : not found

--- app/app.js
+++ app/app.js
@@ -1,56 +1,103 @@
 
- fetch('/api/spotify')
- .then(response => response.json())
- .then(data => {
-   const trackList = document.getElementById('track-list');
-   data.items.forEach(track => {
-    
-     const listItem = document.createElement('li');
-     listItem.classList.add('border', 'hover:border-neutral-content', 'border-2', 'rounded-md','mb-4');
+fetch('/api/spotify')
+  .then(response => response.json())
+  .then(data => {
+    const currentPlaying = data.currentPlaying;
+    const dataTopTracks = data.topTracks;
+    const trackList = document.getElementById('track-list');
 
-     const linkItem = document.createElement('a');
-     linkItem.href = track.external_urls.spotify;
-     linkItem.target = '_blank';
-    
-    
-     const divContainer = document.createElement('div');
-     divContainer.classList.add('p-2','lg:p-4', 'flex', 'items-center');
+    if (currentPlaying.currently_playing_type === 'track') {
 
-    
-     const img = document.createElement('img');
-     img.id = 'cover';
-     img.src = track.album.images[0].url; 
-     img.alt = `Cover image for ${track.name}`;
-     img.classList.add('rounded-md', 'mr-4', 'lg:size-16','size-12');
+      const listItem = document.createElement('li');
+      listItem.classList.add('border', 'hover:border-neutral-content', 'border-2', 'rounded-md', 'mb-4');
 
-     
-     const textContainer = document.createElement('div');
-     textContainer.classList.add('text-sm','lg:text-md');
+      const linkItem = document.createElement('a');
+      linkItem.href = currentPlaying.item.external_urls.spotify;
+      linkItem.target = '_blank';
 
-    
-     const songTitle = document.createElement('p');
-     songTitle.id = 'judul-lagu';
-     songTitle.classList.add('font-medium');
-     songTitle.textContent = track.name;
+      const divContainer = document.createElement('div');
+      divContainer.classList.add('p-2', 'lg:p-4', 'flex', 'items-center');
 
-    
-     const artistName = document.createElement('p');
-     artistName.id = 'nama-artis';
-     artistName.textContent = track.artists.map(artist => artist.name).join(', '); 
+      const img = document.createElement('img');
+      img.id = 'cover';
+      img.src = currentPlaying.item.album.images[0].url;
+      img.alt = `Cover image for ${currentPlaying.item.name}`;
+      img.classList.add('rounded-md', 'mr-4', 'lg:size-16', 'size-12');
 
-     
-     textContainer.appendChild(songTitle);
-     textContainer.appendChild(artistName);
+      const textContainer = document.createElement('div');
+      textContainer.classList.add('text-sm', 'lg:text-md');
 
-     
-     divContainer.appendChild(img);
-     divContainer.appendChild(textContainer);
+      const songTitle = document.createElement('p');
+      songTitle.id = 'judul-lagu';
+      songTitle.classList.add('font-medium');
+      songTitle.textContent = currentPlaying.item.name;
 
-     
-     linkItem.appendChild(divContainer);
-     listItem.appendChild(linkItem);
-     
-     trackList.appendChild(listItem);
-   });
- })
- .catch(error => console.error('Error:', error));
+      const artistName = document.createElement('p');
+      artistName.id = 'nama-artis';
+      
+      artistName.textContent = currentPlaying.item.artists.map(artist => artist.name).join(', ');
+
+      textContainer.appendChild(songTitle);
+      textContainer.appendChild(artistName);
+
+      divContainer.appendChild(img);
+      divContainer.appendChild(textContainer);
+
+      linkItem.appendChild(divContainer);
+
+      listItem.appendChild(linkItem);
+
+      trackList.appendChild(listItem);
+    }
+
+    dataTopTracks.items.forEach(track => {
+
+      const listItem = document.createElement('li');
+      listItem.classList.add('border', 'hover:border-neutral-content', 'border-2', 'rounded-md', 'mb-4');
+
+      const linkItem = document.createElement('a');
+      linkItem.href = track.external_urls.spotify;
+      linkItem.target = '_blank';
+
+
+      const divContainer = document.createElement('div');
+      divContainer.classList.add('p-2', 'lg:p-4', 'flex', 'items-center');
+
+
+      const img = document.createElement('img');
+      img.id = 'cover';
+      img.src = track.album.images[0].url;
+      img.alt = `Cover image for ${track.name}`;
+      img.classList.add('rounded-md', 'mr-4', 'lg:size-16', 'size-12');
+
+
+      const textContainer = document.createElement('div');
+      textContainer.classList.add('text-sm', 'lg:text-md');
+
+
+      const songTitle = document.createElement('p');
+      songTitle.id = 'judul-lagu';
+      songTitle.classList.add('font-medium');
+      songTitle.textContent = track.name;
+
+
+      const artistName = document.createElement('p');
+      artistName.id = 'nama-artis';
+      artistName.textContent = track.artists.map(artist => artist.name).join(', ');
+
+
+      textContainer.appendChild(songTitle);
+      textContainer.appendChild(artistName);
+
+
+      divContainer.appendChild(img);
+      divContainer.appendChild(textContainer);
+
+
+      linkItem.appendChild(divContainer);
+      listItem.appendChild(linkItem);
+
+      trackList.appendChild(listItem);
+    });
+  })
+  .catch(error => console.error('Error:', error));
